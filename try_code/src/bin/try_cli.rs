7a7644c39637b75b7950a66adffd2455d016925a
skellykiernan <skellykiernan@users.noreply.github.com>
2021-09-11 22:04:53 +0100

    Include an optional argument
    For try_cli, added the optional argument plus use a "match" to check the argument.

--- try_code/src/bin/try_cli.rs
+++ try_code/src/bin/try_cli.rs
@@ -4,10 +4,10 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     
-    /// path to register definition file  
+    /// path to register definition file, an optional argument using the std::Option, see https://doc.rust-lang.org/std/option/  
     #[structopt(short, long, parse(from_os_str))]
     defs: Option<std::path::PathBuf>,
-    /// register name
+    /// register name, required positional argument
     reg_name: String,
 }
 
@@ -19,4 +19,18 @@ fn main() {
     println!("args.defs {:?}  args.reg_name {:?}", 
               args.defs, 
               args.reg_name);
+
+    println!("");
+    println!("---- Checking arguments");
+    println!("");
+
+    // Checking an optional argument from the command line
+    match args.defs {
+        Some(x) => println!("There was SOME args.defs {:?}", x),
+        None    => println!("There is no(NONE) args.defs"),
+    }
+
+    // TODO how to handle multiple options
+
+
 }
